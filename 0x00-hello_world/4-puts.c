ae528a6b11775e5dd7a5953b1d3c08399c5fea44
Othman Semlali <othmansemlali@gmail.com>
2023-08-10 08:55:14 -0700

    correct syntax based on betty resp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ * This function prints sentence
+ * using the puts function and then returns 0 to indicate successful execution.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
