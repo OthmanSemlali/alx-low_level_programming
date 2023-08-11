8cec0c96b657843084f48f19f3683b2438d68de0
Othman Semlali <othmansemlali@gmail.com>
2023-08-11 04:48:09 -0700

    correct indentetions

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -25,15 +25,15 @@ n = rand() - RAND_MAX / 2;
     /* Determine and print the nature of the generated number */
 if (n > 0)
 {
-printf("%d : is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%d : is negative\n", n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d : is zero\n", n);
+printf("%d is zero\n", n);
 }
 
 /* Return 0 to indicate successful completion */
