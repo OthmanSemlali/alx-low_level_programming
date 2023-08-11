bd5d87c42eaa8eeb6905caf9600ab7c074ab8a0d
Othman Semlali <othmansemlali@gmail.com>
2023-08-11 07:26:28 -0700

    using continue keyword

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This program prints the lowercase alphabet from 'a' to 'z'  except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+{
+if (i == 'q' || i == 'e')
+{
+continue;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
