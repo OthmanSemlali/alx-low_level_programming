352b520848ebbe7a2e550b8a0bf4c5db49f008ec
Othman Semlali <othmansemlali@gmail.com>
2023-08-17 18:00:47 -0700

    correct

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- *_isdigit -   checks for a digit (0 through 9)
- * @c: digit to check
- * Return: 1 is c digit .otherwise 0
- *
- */
+* _isdigit - check if is digit
+*
+* @c: is the char to be checked
+* Return: 1 if is digit, otherwise 0.
+*/
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
 }
-
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,18 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * print_numbers -  prints the numbers, from 0 to 9,
- * followed by a new line.
+ *print_numbers - func. print from 0 to 9
  * Return: void
- *
  */
+
 void print_numbers(void)
 {
-	int i;
-	for (i=0; i < 10; i++)
-		_putchar(i + '0');
+	int a = 0;
+
+	while (a <= 9)
+	{
+		_putchar(a + '0');
+		a++;
+	}
 	_putchar('\n');
 }
