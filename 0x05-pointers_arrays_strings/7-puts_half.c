af0a120c6dba64a02084405813f6e3dd7ed2e012
Othman Semlali <othmansemlali@gmail.com>
2023-08-21 10:32:12 -0700

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to be printed
+ * Return: void
+ *
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int len;
+
+	while (str[i] != '\n')
+	{
+		i++;
+	}
+
+	len = i;
+
+	if (i % 2 == 0)
+	{
+		i /= 2;
+	}
+	else
+	{
+		i = (i - 1) / 2;
+	}
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
