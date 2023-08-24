c1d6fa2f13b2e74db5d603bd337106f9074a8e5f
Othman Semlali <othmansemlali@gmail.com>
2023-08-24 05:25:17 -0700

    leet - encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to be encoded
+ * Return: encoded string
+ */
+
+char *leet(char *s){
+
+int i = 0;
+int j = 0;
+char *letters = "aAeEoOtTlL";
+char *numbers = "4433007711";
+
+while (s[i] != '\0')
+{
+j = 0;
+while (letters[j] != '\0')
+{
+if (s[i] == letters[j])
+{
+s[i] = numbers[j];
+}
+j++;
+}
+i++;
+}
+return (s);
+}
