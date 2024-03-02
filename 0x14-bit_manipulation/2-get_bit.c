cb47a83abadae07217616f065796e3207cc748d5
OthmanSemlali <othmansemlali23@gmail.com>
2024-03-02 01:24:32 -0800

    get bir func

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: number to get the bit from
+ * @index: index of the bit to get
+ * Return: value of the bit at index or -1 if an error occured
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+return ((n >> index) & 1);
+}
